e2e58315b508d79866d93e84d88e8abb16828e9b
q2q2q8 <art-kash@mail.ru>
2022-11-11 23:26:59 +0300

    проверьте, пожалуйста

---
+++ src/Main.java
@@ -0,0 +1,76 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задача 1, задание 1.
+        int[] numbers = new int [3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        for (int i = 0; i < 3; i++) {
+            System.out.println(numbers[i]);
+        }
+        // Задача 1, задание 2.
+        double[] numbersD = {1.57, 7.654, 9.986};
+        for (int i = 0; i < 3; i++) {
+            System.out.println(numbersD[i]);
+        }
+        // Задача 1, задание 3.
+        int[] numbersX = {4, 5, 6};
+        for (int i = 0; i < 3; i++) {
+            System.out.println(numbersX[i]);
+        }
+        System.out.println("");
+        // Задача 2.
+        int[] numbersQ = new int [3];
+        numbersQ[0] = 1;
+        numbersQ[1] = 2;
+        numbersQ[2] = 3;
+        {
+            System.out.print(numbersQ[0] + ", " + numbersQ[1] + ", " + numbersQ[2]);
+        }
+        System.out.println("");
+        double[] numbersW = {1.57, 7.654, 9.986};
+        {
+            System.out.print(numbersW[0] + ", " + numbersW[1] + ", " + numbersW[2]);
+        }
+        System.out.println("");
+        int[] numbersE = {4, 5, 6};
+        {
+            System.out.print(numbersE[0] + ", " + numbersE[1] + ", " + numbersE[2]);
+        }
+        System.out.println("");
+        // Задача 3.
+        System.out.println("");
+        int[] numbersR = new int [3];
+        numbersR[0] = 1;
+        numbersR[1] = 2;
+        numbersR[2] = 3;
+        {
+            System.out.print(numbersR[2] + ", " + numbersR[1] + ", " + numbersR[0]);
+        }
+        System.out.println("");
+        double[] numbersT = {1.57, 7.654, 9.986};
+        {
+            System.out.print(numbersT[2] + ", " + numbersT[1] + ", " + numbersT[0]);
+        }
+        System.out.println("");
+        int[] numbersY = {4, 5, 6};
+        {
+            System.out.print(numbersY[2] + ", " + numbersY[1] + ", " + numbersY[0]);
+        }
+        System.out.println("");
+        // Задача 4.
+        System.out.println("");
+        int[] numbersA = new int [3];
+        numbersA[0] = 1;
+        numbersA[1] = 2;
+        numbersA[2] = 3;
+        for (int i = 0; i < 3; i++) {
+            System.out.println(numbersA[i]+1);
+        }
+
+
+
+
+
+    }
+}
